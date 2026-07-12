b456dcc751ac93890b16b57e478f5ba61fdf8ed1
Jerry Dean Smith, Jr. <jsmith@jerry-smith.net>
2026-07-11 23:01:52 -0500

    Serve personal site fonts locally

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,5 +1,6 @@
 module.exports = {
   extends: ["stylelint-config-standard"],
+  ignoreFiles: ["site/frontend/css/fontawesome.min.css"],
   rules: {
     "color-no-invalid-hex": true,
     "declaration-block-no-duplicate-properties": true,
